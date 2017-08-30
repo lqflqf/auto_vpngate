aeb8cec3ef90990772ce46c24e326130eba4f2c9
qifan <avanti.lee@gmail.com>
2017-08-30 23:35:45 +0800

    download to local has been done

--- Parser/csv_parser.py
+++ Parser/csv_parser.py
@@ -2,24 +2,21 @@ from Parser.ovpn import Ovpn
 
 class CSV_parser:
     path = None
-    file = None
-    filter = None
+    files = []
 
     def __init__(self):
         self.path = "http://www.vpngate.net/api/iphone/"
+        self.open_remote()
 
 
     def open_remote(self):
         import urllib.request;
         with urllib.request.urlopen(self.path) as response:
             lines = response.read().decode("utf-8").split('\r\n')
-        ofiles = []
         for i in lines[2:-2]:
-            ofiles.append(i)
-        return ofiles
+            self.files.append(Ovpn(i))
 
 
-# csv = CSV_parser()
-# files = csv.open_remote()
-# print(len(files))
+
+
 
---
+++ main.py
@@ -0,0 +1,20 @@
+from Parser import csv_parser
+
+csv_parser = csv_parser.CSV_parser()
+
+print("total file " + str(len(csv_parser.files)))
+
+files = list(filter(lambda f: f.country_short == 'JP' \
+               and f.speed >= 5000000 \
+               and f.uptime >= 86400000, \
+               csv_parser.files))
+
+print("JP file " + str(len(files)))
+
+for f in files:
+    f.save_file('test_files')
+
+
+
+
+
