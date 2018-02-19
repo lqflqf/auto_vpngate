64dcc4bbc6faace193902cf3048846b4080dd75a
qifan <avanti.lee@gmail.com>
2018-02-19 19:43:53 +0800

    output message update

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 from Parser import CsvParser
 from Email import EmailProcessor
+from collections import Counter
 
 csv_parser = CsvParser.CSV_parser()
 
-print("total file " + str(len(csv_parser.files)))
+print("Total file " + str(len(csv_parser.files)))
+print("")
 
 # files = list(filter(lambda f: f.country_short == 'JP' \
 #                and f.speed >= 5000000 \
 #                and f.uptime >= 86400000, \
 #                csv_parser.files))
 
-files = list(filter(lambda f: f.protocol == 'udp' and f.country_short in ['JP', 'US'], csv_parser.files))
+cnt = Counter()
 
-print("files " + str(len(files)))
+for f in csv_parser.files:
+    cnt[f.country_long + ' ' + f.protocol] += 1
 
-for f in files:
+
+for k, v in cnt.items():
+    print(k + ": " + str(v))
+
+files_to_save = list(filter(lambda f: f.protocol in ['udp'] and f.country_short in ['JP', 'US'], csv_parser.files))
+
+
+print("")
+print("saved files " + str(len(files_to_save)))
+
+for f in files_to_save:
     f.save_file('test_files')
 
 
