10f32126e696894b3ce979789828d3516b1df10b
qifan <avanti.lee@gmail.com>
2018-02-24 15:14:39 +0800

    reset master branch

--- main.py
+++ main.py
@@ -17,19 +17,16 @@ cnt = Counter()
 for f in csv_parser.files:
     cnt[f.country_long + ' ' + f.protocol] += 1
 
-
 for k, v in sorted(cnt.items()):
     print(k + ": " + str(v))
 
 files_to_save = list(filter(lambda f: f.protocol in ['udp'] and f.country_short in ['JP', 'US'], csv_parser.files))
-
 
 print("")
 print("saved files " + str(len(files_to_save)))
 
 for f in files_to_save:
     f.save_file('test_files')
-
 
 # if len(files) > 0:
 #     EmailProcessor.processMail(files)
