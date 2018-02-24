9622831232188c2a62256fc5a7d100dd46ff302d
qifan <avanti.lee@gmail.com>
2018-02-24 15:20:02 +0800

    reset

--- main.py
+++ main.py
@@ -17,24 +17,19 @@ cnt = Counter()
 for f in csv_parser.files:
     cnt[f.country_long + ' ' + f.protocol] += 1
 
-
 for k, v in sorted(cnt.items()):
     print(k + ": " + str(v))
 
 files_to_save = list(filter(lambda f: f.protocol in ['udp'] and f.country_short in ['JP', 'US'], csv_parser.files))
 
-
 print("")
 print("saved files " + str(len(files_to_save)))
 
-buffer = EmailProcessor.addToZip(files_to_save)
+for f in files_to_save:
+    f.save_file('test_files')
 
-with open("ovpn_files.zip", "w") as ofile:
-    ofile.write(buffer)
-    ofile.close()
-
-
-print("Done!")
+# if len(files) > 0:
+#     EmailProcessor.processMail(files)
 
 
 
