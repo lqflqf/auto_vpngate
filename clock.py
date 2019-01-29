169afe012bc77c7b866eaa77fcd3761fff975ef4
qifan <avanti.lee@gmail.com>
2019-01-29 18:12:05 +0800

    interval changed

--- clock.py
+++ clock.py
@@ -6,7 +6,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=15)
+@sched.scheduled_job('interval', minutes=10)
 def timed_job():
     c = configuration.Configuration()
 
