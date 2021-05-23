a2bcf3cbf555a6664b3281c375656c814d8bc39b
Qifan Li <avanti.lee@gmail.com>
2021-05-23 19:09:24 +0800

    update dialect in cmd package

--- cmd/create_db.py
+++ cmd/create_db.py
@@ -4,7 +4,7 @@ import db_model
 
 
 if __name__ == '__main__':
-    engine = sqlalchemy.create_engine(os.environ['DATABASE_URL'])
+    engine = sqlalchemy.create_engine(os.environ['DATABASE_URL'].replace("postgres", "postgresql+psycopg2"))
     db_model.Base.metadata.createall(engine)
     print('tables created')
 
--- cmd/drop_db.py
+++ cmd/drop_db.py
@@ -3,7 +3,7 @@ import sqlalchemy
 
 
 if __name__ == '__main__':
-    engine = sqlalchemy.create_engine(os.environ['DATABASE_URL'])
+    engine = sqlalchemy.create_engine(os.environ['DATABASE_URL'].replace("postgres", "postgresql+psycopg2"))
     meta = sqlalchemy.MetaData(bind=engine)
     meta.reflect()
     meta.drop_all()
